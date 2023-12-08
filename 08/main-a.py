24e5db999f9948789345927c8af64987306eb773
Florent <florent@sqooba.io>
2023-12-08 08:59:23 +0100

    well, start on AAA, not the first instruction :o

--- 08/main-a.py
+++ 08/main-a.py
@@ -20,16 +20,13 @@ class Node:
 class Game:
     instructions: str
     nodes: Dict[str, Node]
-    firstNode: Node
 
     def __repr__(self) -> str:
         s = self.instructions + "\n\n"
-        s += str(self.firstNode) + "\n"
 
         for n in self.nodes.values():
-            if n != self.firstNode:
-                s += str(n)
-                s += "\n"
+            s += str(n)
+            s += "\n"
 
         return s
 
@@ -48,18 +45,21 @@ def parse(lines: Iterable[str]) -> Game:
             Node(value=matched.group(1), left=matched.group(2), right=matched.group(3))
         )
 
-    return Game(
-        instructions=instructions, nodes={n.value: n for n in nodes}, firstNode=nodes[0]
-    )
+    return Game(instructions=instructions, nodes={n.value: n for n in nodes})
 
 
 def solve(game: Game) -> int:
     n_hops = 0
-    here = game.firstNode
+    here = game.nodes["AAA"]
 
     while here.value != "ZZZ":
-        is_left_turn = game.instructions[n_hops % len(game.instructions)] == "L"
-        here = game.nodes[here.left if is_left_turn else here.right]
+        match game.instructions[n_hops % len(game.instructions)]:
+            case "L":
+                here = game.nodes[here.left]
+            case "R":
+                here = game.nodes[here.right]
+            case e:
+                raise ValueError(f"unknown instruction: {e}")
         n_hops += 1
 
     return n_hops
